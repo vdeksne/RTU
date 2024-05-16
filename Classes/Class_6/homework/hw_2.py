3b41ce78c042891b8a51a78e3432b4718a5f4d47
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-05-16 09:12:56 +0300

    finalized

--- Classes/Class_6/homework/hw_2.py
+++ Classes/Class_6/homework/hw_2.py
@@ -11,8 +11,16 @@
 # PS teoretiski varētu iztikt bez list, bet ar list būs ērtāk
 
 
-start = int(input("Enter the start number: "))
-end = int(input("Enter the end number: "))
+while True:
+    try:
+        start = int(input("Enter the start number: "))
+        end = int(input("Enter the end number: "))
+        if start > end:
+            print("Start number must be smaller than end number!")
+            continue
+        break
+    except ValueError:
+        print("Both start and end numbers must be integers!")
 
 cubes = []
 for num in range(start, end+1):
@@ -24,35 +32,35 @@ print("Visi kubi:", cubes)
 
 # otra versija
 
-while True:
-    try:
-        start = int(input('Lūdzu ievadiet veselu sākuma skaitli: '))
-        end = int(input('Lūdzu ievadiet veselu beigu skaitli: '))
-        # could add more checks for start < end if you want
-        if start > end:
-            print('Sākuma skaitlim jābūt mazākam par beigu skaitli!')
-            continue # go back to start of loop
-        break
-    except ValueError:
-        print('Gan beigām gan sākumam jābūt veseliem skaitļiem!')
+# while True:
+#     try:
+#         start = int(input('Lūdzu ievadiet veselu sākuma skaitli: '))
+#         end = int(input('Lūdzu ievadiet veselu beigu skaitli: '))
+#         # could add more checks for start < end if you want
+#         if start > end:
+#             print('Sākuma skaitlim jābūt mazākam par beigu skaitli!')
+#             continue # go back to start of loop
+#         break
+#     except ValueError:
+#         print('Gan beigām gan sākumam jābūt veseliem skaitļiem!')
 
 
-visi_kubi = []
-# while x <= y:
-#     print(f'{x}  kubā: {x**3}')
-#     visi_kubi.append(x**3)
-#     x += 1
+# visi_kubi = []
+# # while x <= y:
+# #     print(f'{x}  kubā: {x**3}')
+# #     visi_kubi.append(x**3)
+# #     x += 1
 
-# for loop would be more suitable since we know the range
-for i in range(start, end + 1):
-    print(f'{i}  kubā: {i**3}')
-    visi_kubi.append(i**3)
-# above is a fine approach
+# # for loop would be more suitable since we know the range
+# for i in range(start, end + 1):
+#     print(f'{i}  kubā: {i**3}')
+#     visi_kubi.append(i**3)
+# # above is a fine approach
 
-# we could also do this with list comprehension
-# visi_kubi = [i**3 for i in range(start, end + 1)]
-# # now we print individual values using enumerate
-# for i, kubs in enumerate(visi_kubi, start=start):
-#     print(f'{i} kubā: {kubs}')
+# # we could also do this with list comprehension
+# # visi_kubi = [i**3 for i in range(start, end + 1)]
+# # # now we print individual values using enumerate
+# # for i, kubs in enumerate(visi_kubi, start=start):
+# #     print(f'{i} kubā: {kubs}')
 
-print(f'Visi kubi: {visi_kubi}')
+# print(f'Visi kubi: {visi_kubi}')
--- Classes/Class_6/homework/hw_3.py
+++ Classes/Class_6/homework/hw_3.py
@@ -14,3 +14,21 @@ print(reversed_sentence)
 
 # 4. Pirmskaitļi - šis varētu būt nedēļas nogales uzdevums, klasē diez vai pietiks laika
 # Atrodiet un izvadiet pirmos 20 (vēl labāk iespēja izvēlēties cik pirmos pirmskaitļus gribam) pirmskaitļus saraksta veidā t.i. [2,3,5,7,11,...]
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+num = 2
+while len(primes) < 20:
+    if is_prime(num):
+        primes.append(num)
+    num += 1
+
+print(primes)
+
+# skaldi un valdi uzdevumus, lai būtu vieglāk pārskatāmi
